35a2058ee0b6b144670b20e45fa8042872269135
Chu <mouse170@gmail.com>
2016-03-13 21:16:57 +0800

    isucsie version

--- js/app.jsx
+++ js/app.jsx
@@ -15,9 +15,7 @@ class SampleApp extends React.Component {
   render() {
     return (
       <App
-        googleAnalyticsKey="UA-42490151-3"
-        segmentKey="Pd3LXILLoxlOKXi9zWTCyhK2MRvygFhF"
-        stripeKey="pk_BkaOyHcEiCFaUiEADe7UH6Wq7D6f7"
+        googleAnalyticsKey="UA-69988115-1"
         history={history}>
         {this.props.children}
       </App>
